c6cca8026159873260a511600e49a70e3db0b45d
Alexander Corredor <alexanderjose31@gmail.com>
2023-08-24 11:55:59 -0500

    Login Ok

--- principal/urls.py
+++ principal/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from principal.views import inicio_sesion, menu_ppal, rutero, cap_guia, seg_cliente, proy_cumpl, eje_acu, form_gcom
+from principal.views import logout_user, menu_ppal, rutero, cap_guia, seg_cliente, proy_cumpl, eje_acu, form_gcom
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio_sesion, name='inicio'),
-    path('usuarios/',include('usuarios.urls')),
-    path('ciudades/',include('ciudades.urls')),
-    path('clientes/',include('clientes.urls')),
-    path('guia/',include('guia.urls')),
+
+    path('usuarios/', include('usuarios.urls')),
+    path('ciudades/', include('ciudades.urls')),
+    path('clientes/', include('clientes.urls')),
+    path('guia/', include('guia.urls')),
     path('menu/', menu_ppal, name=('menu-ppal')),
     path('rutero/', rutero, name='rutero'),
     path('cap-guia/', cap_guia, name=('cap-guia')),
@@ -16,4 +17,8 @@ urlpatterns = [
     path('proy-cumpl/', proy_cumpl, name=('proy-cumpl')),
     path('eje-acu/', eje_acu, name=('eje-acu')),
     path('form-gcom/', form_gcom, name=('form-gcom')),
+
+    path('', auth_views.LoginView.as_view(), name='inicio'),
+    path('logout/', logout_user, name="logout"),
+
 ]
